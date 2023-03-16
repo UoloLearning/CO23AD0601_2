b0dc449fbd14d34420e7edad2b7ccdc3478084a4
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-16 16:13:03 +0530

    Create CO23AD0601_2_A2.py

---
+++ CO23AD0601_2_A2.py
@@ -0,0 +1,3 @@
+print("The tiger is the largest living cat species", end=".") 
+print("It is most recognisable for its dark vertical stripes on orange fur with a white underside",end=".")
+print("An apex predator, it primarily preys on ungulates, such as deer and wild boar",end=".")
